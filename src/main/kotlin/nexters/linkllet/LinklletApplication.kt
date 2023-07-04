20c0038a9ab19e397a52bd8c660e9f536c0ae784
jiwoo <zbqmgldjfh@gmail.com>
2023-07-04 22:30:16 +0900

    init: 초기 프로젝트 설정

---
+++ src/main/kotlin/nexters/linkllet/LinklletApplication.kt
@@ -0,0 +1,11 @@
+package nexters.linkllet
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LinklletApplication
+
+fun main(args: Array<String>) {
+    runApplication<LinklletApplication>(*args)
+}
