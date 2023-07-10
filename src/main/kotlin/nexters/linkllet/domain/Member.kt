0ae7976eaa462295a0f6f656ca690e6d0952c279
kth990303 <kth990303@naver.com>
2023-07-10 17:32:28 +0900

    feat: 회원 도메인 JPA 설정 추가

--- src/main/kotlin/nexters/linkllet/domain/Member.kt
+++ src/main/kotlin/nexters/linkllet/domain/Member.kt
@@ -1,10 +1,14 @@
 package nexters.linkllet.domain
 
 import javax.persistence.Column
+import javax.persistence.Entity
 import javax.persistence.GeneratedValue
 import javax.persistence.GenerationType
 import javax.persistence.Id
+import javax.persistence.Table
 
+@Entity
+@Table(name = "member")
 class Member(
 
     @Column(name = "device_id", nullable = false)
