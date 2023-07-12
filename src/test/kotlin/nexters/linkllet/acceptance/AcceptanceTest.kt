58083e9da30edcc509fdea17c2e6447a103cb1cd
jiwoo <zbqmgldjfh@gmail.com>
2023-07-12 15:40:28 +0900

    feat(DatabaseLoader): 인수테스트의 초기화 데이터 삽입을 위한 Loader 구현

--- src/test/kotlin/nexters/linkllet/acceptance/AcceptanceTest.kt
+++ src/test/kotlin/nexters/linkllet/acceptance/AcceptanceTest.kt
@@ -3,20 +3,25 @@ package nexters.linkllet.acceptance
 import io.restassured.RestAssured
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.web.server.LocalServerPort
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ExtendWith(DatabaseCleanerCallback::class)
-class AcceptanceTest {
+class AcceptanceTest() {
 
     @LocalServerPort
     var port = 80
+
+    @Autowired
+    private lateinit var databaseLoader: DatabaseLoader
 
     @BeforeEach
     fun setUp() {
         if (RestAssured.port == RestAssured.UNDEFINED_PORT) {
             RestAssured.port = port
         }
+        databaseLoader.loadData()
     }
 }
---
+++ src/test/kotlin/nexters/linkllet/acceptance/DatabaseLoader.kt
@@ -0,0 +1,25 @@
+package nexters.linkllet.acceptance
+
+import nexters.linkllet.member.domain.Member
+import nexters.linkllet.member.domain.MemberRepository
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.stereotype.Component
+
+@Component
+class DatabaseLoader(
+    private val memberRepository: MemberRepository,
+) {
+
+    companion object {
+        private val log: Logger = LoggerFactory.getLogger(this.javaClass)!!
+    }
+
+    fun loadData() {
+        log.debug("[call DataLoader]")
+
+        memberRepository.save(Member("device_id"))
+
+        log.debug("[init complete DataLoader]")
+    }
+}
