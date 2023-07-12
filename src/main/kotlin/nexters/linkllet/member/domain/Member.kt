5901a088b87cd7ff748a23bfb7291cf14664c980
jiwoo <zbqmgldjfh@gmail.com>
2023-07-12 11:35:06 +0900

    refactor: 클래스 페키지 이동

--- src/main/kotlin/nexters/linkllet/member/domain/Member.kt
+++ src/main/kotlin/nexters/linkllet/member/domain/Member.kt
@@ -1,31 +1,22 @@
 package nexters.linkllet.member.domain
 
-import nexters.linkllet.common.domain.BaseTimeEntity
-import javax.persistence.*
+import javax.persistence.Column
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.persistence.Id
+import javax.persistence.Table
 
 @Entity
 @Table(name = "member")
 class Member(
 
     @Column(name = "device_id", nullable = false)
-    private val deviceId: String,
+    val deviceId: String,
 
     @Id
     @Column(name = "member_id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private val id: Long = 0L,
-) : BaseTimeEntity() {
-
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return false
-
-        other as Member
-
-        return id == other.id
-    }
-
-    override fun hashCode(): Int {
-        return id.hashCode()
-    }
+    val id: Long = 0L,
+) {
 }
