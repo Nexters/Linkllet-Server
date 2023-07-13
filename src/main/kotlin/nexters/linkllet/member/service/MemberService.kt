d310b2e4d641d82ff615587d1155dcf7fc1ef99b
jiwoo <zbqmgldjfh@gmail.com>
2023-07-13 20:46:59 +0900

    feat: 회원 가입시 기본폴더도 바로 생성되도록 로직 변경

--- src/main/kotlin/nexters/linkllet/member/service/MemberService.kt
+++ src/main/kotlin/nexters/linkllet/member/service/MemberService.kt
@@ -1,17 +1,24 @@
 package nexters.linkllet.member.service
 
+import nexters.linkllet.folder.domain.Folder
+import nexters.linkllet.folder.domain.FolderRepository
+import nexters.linkllet.folder.domain.FolderType
 import nexters.linkllet.member.domain.Member
 import nexters.linkllet.member.domain.MemberRepository
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
+private const val DEFAULT_FOLDER_NAME = "default"
+
 @Transactional
 @Service
 class MemberService(
     private val memberRepository: MemberRepository,
+    private val folderRepository: FolderRepository,
 ) {
 
     fun signUp(deviceId: String) {
-        memberRepository.save(Member(deviceId))
+        val newMember = memberRepository.save(Member(deviceId))
+        folderRepository.save(Folder(DEFAULT_FOLDER_NAME, newMember.id, FolderType.DEFAULT))
     }
 }
