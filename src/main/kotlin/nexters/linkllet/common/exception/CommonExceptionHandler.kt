ff52130c56484fd259705a27af22dc585c95de3e
jiwoo <zbqmgldjfh@gmail.com>
2023-07-13 11:00:12 +0900

    feat: IllegalStateException에 대한 공용 처리 헨들러 작성

--- src/main/kotlin/nexters/linkllet/common/exception/CommonExceptionHandler.kt
+++ src/main/kotlin/nexters/linkllet/common/exception/CommonExceptionHandler.kt
@@ -1,11 +1,11 @@
-package nexters.linkllet.folder.exception
+package nexters.linkllet.common.exception
 
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
 
 @RestControllerAdvice
-class FolderExceptionHandler {
+class CommonExceptionHandler {
 
     @ExceptionHandler
     fun IllegalStateExceptionHandler(ex: IllegalStateException): ResponseEntity<String> {
