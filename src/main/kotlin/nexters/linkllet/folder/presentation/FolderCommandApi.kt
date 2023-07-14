398c4ffd3aa27d6bb6072f88e0c958316073816d
kth990303 <kth990303@naver.com>
2023-07-14 18:19:06 +0900

    feat: `@RequestHeader` 대신 `@AccessDeviceId` 어노테이션을 통해 헤더 추출하도록 구현

--- src/main/kotlin/nexters/linkllet/folder/presentation/FolderCommandApi.kt
+++ src/main/kotlin/nexters/linkllet/folder/presentation/FolderCommandApi.kt
@@ -1,5 +1,6 @@
 package nexters.linkllet.folder.presentation
 
+import nexters.linkllet.common.support.AccessDeviceId
 import nexters.linkllet.folder.dto.ArticleCreateRequest
 import nexters.linkllet.folder.dto.FolderCreateRequest
 import nexters.linkllet.folder.service.FolderService
@@ -9,17 +10,13 @@ import org.springframework.web.bind.annotation.*
 @RestController
 @RequestMapping("/api/v1/folders")
 class FolderCommandApi(
-    private val folderService: FolderService,
+        private val folderService: FolderService,
 ) {
-
-    companion object {
-        private const val DEVICE_ID_HEADER_KEY: String = "Device-Id"
-    }
 
     @PostMapping
     fun createFolder(
-        @RequestBody request: FolderCreateRequest,
-        @RequestHeader(DEVICE_ID_HEADER_KEY) deviceId: String,
+            @RequestBody request: FolderCreateRequest,
+            @AccessDeviceId deviceId: String,
     ): ResponseEntity<Unit> {
         folderService.createFolder(request.name, deviceId)
         return ResponseEntity.ok().build()
@@ -27,8 +24,8 @@ class FolderCommandApi(
 
     @DeleteMapping("/{id}")
     fun deleteFolder(
-        @PathVariable id: Long,
-        @RequestHeader(DEVICE_ID_HEADER_KEY) deviceId: String,
+            @PathVariable id: Long,
+            @AccessDeviceId deviceId: String,
     ): ResponseEntity<Unit> {
         folderService.deleteFolder(id, deviceId)
         return ResponseEntity.noContent().build()
@@ -36,9 +33,9 @@ class FolderCommandApi(
 
     @PostMapping("/{id}/articles")
     fun createArticle(
-        @PathVariable id: Long,
-        @RequestBody request: ArticleCreateRequest,
-        @RequestHeader(DEVICE_ID_HEADER_KEY) deviceId: String,
+            @PathVariable id: Long,
+            @RequestBody request: ArticleCreateRequest,
+            @AccessDeviceId deviceId: String,
     ): ResponseEntity<Unit> {
         folderService.addArticleAtFolder(id, request.name, request.url, deviceId)
         return ResponseEntity.ok().build()
@@ -46,9 +43,9 @@ class FolderCommandApi(
 
     @DeleteMapping("/{id}/articles/{articleId}")
     fun createArticle(
-        @PathVariable id: Long,
-        @PathVariable articleId: Long,
-        @RequestHeader(DEVICE_ID_HEADER_KEY) deviceId: String,
+            @PathVariable id: Long,
+            @PathVariable articleId: Long,
+            @AccessDeviceId deviceId: String,
     ): ResponseEntity<Unit> {
         folderService.deleteArticleAtFolder(id, articleId, deviceId)
         return ResponseEntity.noContent().build()
--- src/main/kotlin/nexters/linkllet/folder/presentation/FolderQueryApi.kt
+++ src/main/kotlin/nexters/linkllet/folder/presentation/FolderQueryApi.kt
@@ -1,5 +1,6 @@
 package nexters.linkllet.folder.presentation
 
+import nexters.linkllet.common.support.AccessDeviceId
 import nexters.linkllet.folder.dto.ArticleLookupListResponse
 import nexters.linkllet.folder.dto.FolderLookupListResponse
 import nexters.linkllet.folder.service.FolderService
@@ -9,16 +10,12 @@ import org.springframework.web.bind.annotation.*
 @RestController
 @RequestMapping("/api/v1/folders")
 class FolderQueryApi(
-    private val folderService: FolderService,
+        private val folderService: FolderService,
 ) {
-
-    companion object {
-        private const val DEVICE_ID_HEADER_KEY: String = "Device-Id"
-    }
 
     @GetMapping
     fun lookUpFolderList(
-        @RequestHeader(DEVICE_ID_HEADER_KEY) deviceId: String,
+            @AccessDeviceId deviceId: String,
     ): ResponseEntity<FolderLookupListResponse> {
         val lookupDtoList = folderService.lookupFolderList(deviceId)
         return ResponseEntity.ok().body(lookupDtoList)
@@ -26,8 +23,8 @@ class FolderQueryApi(
 
     @GetMapping("/{id}/articles")
     fun lookUpArticleList(
-        @PathVariable id: Long,
-        @RequestHeader(DEVICE_ID_HEADER_KEY) deviceId: String,
+            @PathVariable id: Long,
+            @AccessDeviceId deviceId: String,
     ): ResponseEntity<ArticleLookupListResponse> {
         val lookupDtoList = folderService.lookupArticleList(id, deviceId)
         return ResponseEntity.ok().body(lookupDtoList)
