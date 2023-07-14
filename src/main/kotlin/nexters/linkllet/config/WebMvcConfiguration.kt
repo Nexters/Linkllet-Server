f492b4f62667ef59aa2be04033618245f5343df1
kth990303 <kth990303@naver.com>
2023-07-14 18:21:03 +0900

    feat: cors 이슈 해결 및 패키지, 클래스명 변경
    `Config` -> `Configuration`

--- src/main/kotlin/nexters/linkllet/config/WebMvcConfiguration.kt
+++ src/main/kotlin/nexters/linkllet/config/WebMvcConfiguration.kt
@@ -1,16 +1,23 @@
-package nexters.linkllet.common.config
+package nexters.linkllet.config
 
 import nexters.linkllet.common.support.DeviceUserResolver
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.method.support.HandlerMethodArgumentResolver
+import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
-class WebMvcConfig(
+class WebMvcConfiguration(
         val deviceUserResolver: DeviceUserResolver,
 ) : WebMvcConfigurer {
 
     override fun addArgumentResolvers(resolvers: MutableList<HandlerMethodArgumentResolver>) {
         resolvers.add(deviceUserResolver)
+    }
+
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("*")
     }
 }
