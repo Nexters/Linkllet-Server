3e630f720e67d75ca0c292b6659c1222ba3e2363
kth990303 <kth990303@naver.com>
2023-07-14 19:01:32 +0900

    feat: API 스웨거 설정 추가

--- src/main/kotlin/nexters/linkllet/folder/presentation/FolderCommandApi.kt
+++ src/main/kotlin/nexters/linkllet/folder/presentation/FolderCommandApi.kt
@@ -1,54 +1,66 @@
 package nexters.linkllet.folder.presentation
 
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.security.SecurityRequirement
+import io.swagger.v3.oas.annotations.tags.Tag
 import nexters.linkllet.folder.dto.ArticleCreateRequest
 import nexters.linkllet.folder.dto.FolderCreateRequest
 import nexters.linkllet.folder.service.FolderService
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
+@Tag(name = "Folders-Command", description = "폴더 작업")
 @RestController
 @RequestMapping("/api/v1/folders")
 class FolderCommandApi(
-    private val folderService: FolderService,
+        private val folderService: FolderService,
 ) {
 
     companion object {
         private const val DEVICE_ID_HEADER_KEY: String = "Device-Id"
     }
 
+    @Operation(summary = "폴더 생성")
+    @SecurityRequirement(name = "DeviceId")
     @PostMapping
     fun createFolder(
-        @RequestBody request: FolderCreateRequest,
-        @RequestHeader(DEVICE_ID_HEADER_KEY) deviceId: String,
+            @RequestBody request: FolderCreateRequest,
+            @RequestHeader(DEVICE_ID_HEADER_KEY) deviceId: String,
     ): ResponseEntity<Unit> {
         folderService.createFolder(request.name, deviceId)
         return ResponseEntity.ok().build()
     }
 
+    @Operation(summary = "폴더 삭제")
+    @SecurityRequirement(name = "DeviceId")
     @DeleteMapping("/{id}")
     fun deleteFolder(
-        @PathVariable id: Long,
-        @RequestHeader(DEVICE_ID_HEADER_KEY) deviceId: String,
+            @PathVariable id: Long,
+            @RequestHeader(DEVICE_ID_HEADER_KEY) deviceId: String,
     ): ResponseEntity<Unit> {
         folderService.deleteFolder(id, deviceId)
         return ResponseEntity.noContent().build()
     }
 
+    @Operation(summary = "링크 생성")
+    @SecurityRequirement(name = "DeviceId")
     @PostMapping("/{id}/articles")
     fun createArticle(
-        @PathVariable id: Long,
-        @RequestBody request: ArticleCreateRequest,
-        @RequestHeader(DEVICE_ID_HEADER_KEY) deviceId: String,
+            @PathVariable id: Long,
+            @RequestBody request: ArticleCreateRequest,
+            @RequestHeader(DEVICE_ID_HEADER_KEY) deviceId: String,
     ): ResponseEntity<Unit> {
         folderService.addArticleAtFolder(id, request.name, request.url, deviceId)
         return ResponseEntity.ok().build()
     }
 
+    @Operation(summary = "링크 삭제")
+    @SecurityRequirement(name = "DeviceId")
     @DeleteMapping("/{id}/articles/{articleId}")
-    fun createArticle(
-        @PathVariable id: Long,
-        @PathVariable articleId: Long,
-        @RequestHeader(DEVICE_ID_HEADER_KEY) deviceId: String,
+    fun deleteArticle(
+            @PathVariable id: Long,
+            @PathVariable articleId: Long,
+            @RequestHeader(DEVICE_ID_HEADER_KEY) deviceId: String,
     ): ResponseEntity<Unit> {
         folderService.deleteArticleAtFolder(id, articleId, deviceId)
         return ResponseEntity.noContent().build()
--- src/main/kotlin/nexters/linkllet/folder/presentation/FolderQueryApi.kt
+++ src/main/kotlin/nexters/linkllet/folder/presentation/FolderQueryApi.kt
@@ -1,33 +1,41 @@
 package nexters.linkllet.folder.presentation
 
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.security.SecurityRequirement
+import io.swagger.v3.oas.annotations.tags.Tag
 import nexters.linkllet.folder.dto.ArticleLookupListResponse
 import nexters.linkllet.folder.dto.FolderLookupListResponse
 import nexters.linkllet.folder.service.FolderService
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
+@Tag(name = "Folders-Query", description = "폴더 조회")
 @RestController
 @RequestMapping("/api/v1/folders")
 class FolderQueryApi(
-    private val folderService: FolderService,
+        private val folderService: FolderService,
 ) {
 
     companion object {
         private const val DEVICE_ID_HEADER_KEY: String = "Device-Id"
     }
 
+    @Operation(summary = "폴더 목록 조회")
+    @SecurityRequirement(name = "DeviceId")
     @GetMapping
     fun lookUpFolderList(
-        @RequestHeader(DEVICE_ID_HEADER_KEY) deviceId: String,
+            @RequestHeader(DEVICE_ID_HEADER_KEY) deviceId: String,
     ): ResponseEntity<FolderLookupListResponse> {
         val lookupDtoList = folderService.lookupFolderList(deviceId)
         return ResponseEntity.ok().body(lookupDtoList)
     }
 
+    @Operation(summary = "기사 목록 조회")
+    @SecurityRequirement(name = "DeviceId")
     @GetMapping("/{id}/articles")
     fun lookUpArticleList(
-        @PathVariable id: Long,
-        @RequestHeader(DEVICE_ID_HEADER_KEY) deviceId: String,
+            @PathVariable id: Long,
+            @RequestHeader(DEVICE_ID_HEADER_KEY) deviceId: String,
     ): ResponseEntity<ArticleLookupListResponse> {
         val lookupDtoList = folderService.lookupArticleList(id, deviceId)
         return ResponseEntity.ok().body(lookupDtoList)
