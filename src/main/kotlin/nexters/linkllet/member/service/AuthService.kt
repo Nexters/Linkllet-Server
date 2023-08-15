73089dcbb76c39097cf1cc59b43937d09e76e16f
kth990303 <kth990303@naver.com>
2023-08-15 18:51:33 +0900

    feat: 애플 OAuth API 구현

--- src/main/kotlin/nexters/linkllet/member/service/AuthService.kt
+++ src/main/kotlin/nexters/linkllet/member/service/AuthService.kt
@@ -1,6 +1,9 @@
 package nexters.linkllet.member.service
 
 import nexters.linkllet.common.support.JwtProvider
+import nexters.linkllet.common.support.apple.AppleOAuthUserProvider
+import nexters.linkllet.member.dto.AppleLoginRequest
+import nexters.linkllet.member.dto.AppleLoginResponse
 import nexters.linkllet.member.dto.LoginRequest
 import nexters.linkllet.member.dto.LoginResponse
 import org.springframework.stereotype.Service
@@ -8,10 +11,17 @@ import org.springframework.stereotype.Service
 @Service
 class AuthService(
         private val jwtTokenProvider: JwtProvider,
+        private val appleOAuthUserProvider: AppleOAuthUserProvider,
 ) {
 
     fun login(request: LoginRequest): LoginResponse {
         val token = jwtTokenProvider.generateToken(request.email)
         return LoginResponse(token)
+    }
+
+    fun loginApple(request: AppleLoginRequest): AppleLoginResponse {
+        val applePlatformUser = appleOAuthUserProvider.getApplePlatformUser(request.token)
+        val token = jwtTokenProvider.generateToken(applePlatformUser.email)
+        return AppleLoginResponse(token)
     }
 }
