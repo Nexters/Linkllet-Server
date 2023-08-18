d7f82d969b32bfcc082f6fc3f90ea3bd3a64a648
jiwoo <zbqmgldjfh@gmail.com>
2023-08-18 11:18:19 +0900

    refactor(KakaoOAuthUserProvider): 필요없는 코드 제거

--- src/main/kotlin/nexters/linkllet/common/support/kakao/KakaoClientResponse.kt
+++ src/main/kotlin/nexters/linkllet/common/support/kakao/KakaoClientResponse.kt
@@ -29,22 +29,3 @@ data class KakaoAccessToken(
         private const val AUTHORIZATION_BEARER = "Bearer "
     }
 }
-
-data class KakaoAccessTokenRequest(
-    val code: String,
-    val client_id: String,
-    val redirect_uri: String,
-    val grant_type: String = "authorization_code",
-) {
-    override fun toString(): String {
-        return "code=" + code + '&' +
-                "client_id=" + client_id + '&' +
-                "redirect_uri=" + redirect_uri + '&' +
-                "grant_type=" + grant_type;
-    }
-}
-
-data class KakaoUserRequest(
-    @JsonProperty("property_keys") private val propertyKeys: String,
-    @JsonProperty("secure_resource") private val secureResource: Boolean = true,
-)
--- src/main/kotlin/nexters/linkllet/common/support/kakao/KakaoOAuthUserProvider.kt
+++ src/main/kotlin/nexters/linkllet/common/support/kakao/KakaoOAuthUserProvider.kt
@@ -1,19 +1,10 @@
 package nexters.linkllet.common.support.kakao
 
-import com.fasterxml.jackson.databind.ObjectMapper
 import feign.FeignException
 import nexters.linkllet.common.exception.dto.UnauthorizedException
 import nexters.linkllet.common.support.kakao.client.KakaoAccessTokenClient
 import nexters.linkllet.common.support.kakao.client.KakaoUserInfoClient
-import org.springframework.http.HttpEntity
-import org.springframework.http.HttpHeaders
-import org.springframework.http.HttpMethod
-import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Component
-import org.springframework.util.LinkedMultiValueMap
-import org.springframework.util.MultiValueMap
-import org.springframework.web.client.RestTemplate
-import org.springframework.web.client.exchange
 
 
 @Component
@@ -41,20 +32,5 @@ class KakaoOAuthUserProvider(
         } catch (e: FeignException) {
             throw UnauthorizedException("Kakao OAuth 인가코드 값이 올바르지 않습니다.")
         }
-    }
-
-    private fun createHeaders(): HttpHeaders? {
-        val headers = HttpHeaders()
-        headers.add("Content-type", "application/x-www-form-urlencoded;charset=utf-8")
-        return headers
-    }
-
-    private fun createParams(code: String): MultiValueMap<String, String>? {
-        val params: MultiValueMap<String, String> = LinkedMultiValueMap()
-        params.add("grant_type", "authorization_code")
-        params.add("client_id", kakaoOAuthConfigProperties.clientId)
-        params.add("redirect_uri", kakaoOAuthConfigProperties.redirectUri)
-        params.add("code", code)
-        return params
     }
 }
