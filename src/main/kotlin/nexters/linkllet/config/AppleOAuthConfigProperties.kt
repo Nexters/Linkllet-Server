38b7448dbdae0e99e1dc4af392263336a7ec5656
kth990303 <kth990303@naver.com>
2023-08-23 11:29:35 +0900

    feat: apple oauth nonce 제거

---
+++ src/main/kotlin/nexters/linkllet/config/AppleOAuthConfigProperties.kt
@@ -0,0 +1,9 @@
+package nexters.linkllet.config
+
+import org.springframework.boot.context.properties.ConfigurationProperties
+
+@ConfigurationProperties(prefix = "oauth.apple")
+data class AppleOAuthConfigProperties(
+    var iss: String = "iss",
+    var clientId: String = "aud"
+)
