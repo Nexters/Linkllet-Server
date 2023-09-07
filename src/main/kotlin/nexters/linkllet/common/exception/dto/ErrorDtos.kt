46b3f529c3fc8d551cfd92ecba76ef6e81d57171
jiwoo <zbqmgldjfh@gmail.com>
2023-09-07 21:39:46 +0900

    refactor: 등록된 사용자의 예외 메시지 변경

--- src/main/kotlin/nexters/linkllet/common/exception/dto/ErrorDtos.kt
+++ src/main/kotlin/nexters/linkllet/common/exception/dto/ErrorDtos.kt
@@ -20,5 +20,4 @@ class ForbiddenException(message: String = "권한이 존재하지 않습니다.
 class NotFoundException(message: String = "존재하지 않는 요청입니다.")
     : LinklletException(message, HttpStatus.NOT_FOUND.value())
 
-class ConflictException(message: String = "이미 등록된 이메일입니다.")
-    : LinklletException(message, HttpStatus.CONFLICT.value())
+class ConflictException(message: String = "이미 등록된 사용자 입니다.") : LinklletException(message, HttpStatus.CONFLICT.value())
